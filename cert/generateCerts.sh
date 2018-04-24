90c2d2fdb19db561595553819776b5992190d39a
Chris Kingdon <ckchessmaster@gmail.com>
2018-04-24 18:20:59 -0400

    updating generateCerts

--- cert/generateCerts.sh
+++ cert/generateCerts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "Generating server cert..."
 openssl req -newkey rsa:4096 -nodes -keyout server_key.pem -x509 -days 365 -out server_cert.pem
+echo "Generating client cert..."
 openssl req -newkey rsa:2048 -nodes -keyout client_key.pem -x509 -days 365 -out client_cert.pem
